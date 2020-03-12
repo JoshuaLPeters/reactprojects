cafc94ff34cac4e55bd8d406fd7433142dc230d0
Joshua Peters <peters.l.joshua@gmail.com>
2020-03-11 19:44:34 -0500

    saving progress with first React Projects with npm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Person(){
+    return(
+        <div className="person">
+            <h1>Josh</h1>
+            <p>Your age: 32</p>
+        </div>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Person />,
+    document.querySelector('#p1'));
