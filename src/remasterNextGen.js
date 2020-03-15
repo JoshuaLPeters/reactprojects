ff4f091ea1e0c00208efdafdd20f4b318224c48d
Joshua Peters <peters.l.joshua@gmail.com>
2020-03-15 00:08:08 -0500

    first practice with Object Oriented JavaScript

--- src/remasterNextGen.js
+++ src/remasterNextGen.js
@@ -1,12 +1,12 @@
-function printMyName(name){
-    console.log(name);
-}
-printMyName("Josh");
+// function printMyName(name){
+//     console.log(name);
+// }
+// printMyName("Josh");
 
 
 // Arrow functions
-const multiply = (number) => number *2;
-console.log(multiply(2));
+// const multiply = (number) => number *2;
+// console.log(multiply(2));
 
 // Exports and Imports (Modules)
 
@@ -26,3 +26,28 @@ console.log(multiply(2));
 // utility.js
 // export const clean = () => {...}
 // export const baseData = 10;
+
+// JavaScript Classes - First example of Object Oriented JavaScript
+
+class Human {
+    constructor() {
+        this.gender = 'male';
+    }
+    printGender(){
+        console.log(this.gender);
+    }
+
+}
+class Person extends Human{
+    constructor() {
+        super();
+        this.name = 'Max';
+    }
+
+    printMyName(){
+        console.log(this.name);
+    }
+}
+const person = new Person();
+person.printMyName();
+person.printGender();
