5214e7a7075befb90c28d91160abe93aa93764af
Joshua Peters <peters.l.joshua@gmail.com>
2020-03-15 00:40:26 -0500

    last save for javascript review on remaster

--- src/remasterNextGen.js
+++ src/remasterNextGen.js
@@ -56,21 +56,50 @@
 // Spread used to split up array elements OR object properties
 // Rest Used to merge a list of function arguments into an array
 
-const numbers = [1, 2, 3];
-const newNumbers = [...numbers, 4];
-console.log(newNumbers);
+// const numbers = [1, 2, 3];
+// const newNumbers = [...numbers, 4];
+// console.log(newNumbers);
+//
+// const person = {
+//     name: 'Max'
+// };
+//
+// const newPerson = {
+//     ...person,
+//     age: 28
+// };
+// console.log(newPerson);
+//
+// const filter = (...args) => {
+//     return args.filter(el => el === 1);
+// };
+// console.log(filter(1,2,3));
 
+// Destructuring
+// Easily extra array elements or object properties and store them in variables
+// const numbers = [1, 2, 3];
+// [num1, ,num3] = numbers;
+// console.log(num1, num3);
+
+// Review Reference and Primative types
+const number = 1;
+const num2 = number; // this copies the number 1 since you are copying the value
+
+
+// Objects and arrays are reference types; copying the pointer
 const person = {
-    name: 'Max'
+    name:'Max'
 };
 
-const newPerson = {
-    ...person,
-    age: 28
-};
-console.log(newPerson);
+const secondPerson = person;
+person.name = 'Manu';
+console.log(secondPerson);
 
-const filter = (...args) => {
-    return args.filter(el => el === 1);
-};
-console.log(filter(1,2,3));
+// Array function review
+const numbers = [1, 2, 3];
+const doubleNumArray = numbers.map((num) => {
+    return num * 2;
+    // returns 2, 4, and 6 as a new array
+});
+console.log(numbers);
+console.log(doubleNumArray);
